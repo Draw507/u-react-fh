07a19666562ce2cf556e4a37f639f3eaf1ba7b03
David Robinson <david.robinson@logicstudio.net>
2020-11-28 08:58:04 -0500

    feat(counter): default props

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,17 +2,21 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 // functional components
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
     return (
         <Fragment>
             <h1>{ saludo }</h1>
-            <p>Mi primera aplicación</p>
+            <p>{ subtitulo }</p>
         </Fragment>
     );
 }
 
 PrimeraApp.prototypes = {
     saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 
 export default PrimeraApp;
