51cd7b01cd59b035964da36d598ab950972f93a3
David Robinson <david.robinson@logicstudio.net>
2020-11-28 09:22:17 -0500

    feat(counter): onclick

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,11 +2,16 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ( { value } ) => {
+
+    const handleAdd = () => {
+        console.log('+1');
+    };
+
     return (
     <Fragment>
         <h1>Counter App</h1>
         <h2>{ value }</h2>
-        <button>+1</button>
+        <button onClick={ handleAdd }>+1</button>
     </Fragment>);
 }
 
