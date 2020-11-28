7859dbe69aeb8395fe52469dc278329b825b76c4
David Robinson <david.robinson@logicstudio.net>
2020-11-28 09:37:59 -0500

    feaf(counter): useState reset

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,12 +1,20 @@
 import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ( { value } ) => {
+const CounterApp = ( { value = 5 } ) => {
     
-    const [ counter, setCounter ] = useState(0);
+    const [ counter, setCounter ] = useState(value);
 
     const handleAdd = () => {
         setCounter(counter + 1);
+    };
+
+    const handleSubtract = () => {
+        setCounter(counter - 1);
+    };
+
+    const handleReset = () => {
+        setCounter(value);
     };
 
     return (
@@ -14,6 +22,8 @@ const CounterApp = ( { value } ) => {
         <h1>Counter App</h1>
         <h2>{ counter }</h2>
         <button onClick={ handleAdd }>+1</button>
+        <button onClick={ handleReset }>Reset</button>
+        <button onClick={ handleSubtract }>-1</button>
     </Fragment>);
 }
 
