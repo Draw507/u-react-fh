fd2b375d7710f0fa621e35c5d523035994ea949c
David Robinson <david.robinson@logicstudio.net>
2020-11-28 19:54:12 -0500

    pruebas enzyme: toMatchSnapshot

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,12 +1,21 @@
 import PrimeraApp from '../PrimeraApp';
+import { shallow } from 'enzyme';
 const { render } = require("@testing-library/react");
 
 describe('Pruebas en PrimeraApp', () => {
-    test('debe mostrar el mensaje Hola soy David', () => {
+    // test('debe mostrar el mensaje Hola soy David', () => {
+    //     const saludo = 'Hola soy David';
+    //     const {getByText} = render(<PrimeraApp saludo = 'Hola soy David' />);
+    //     expect(getByText(saludo)).toBeInTheDocument(); 
+    // })    
+    
+    test('debe de mostrar PrimeraApp correctamente ', () => {
+        
         const saludo = 'Hola soy David';
+        const wrapper = shallow( <PrimeraApp saludo = { saludo } /> );
 
-        const {getByText} = render(<PrimeraApp saludo = 'Hola soy David' />);
-
-        expect(getByText(saludo)).toBeInTheDocument(); 
-    })        
+        expect(wrapper).toMatchSnapshot();
+        
+    })
+    
 })
